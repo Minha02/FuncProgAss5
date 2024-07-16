82fb02163c149e9a651a2c707fe3cb4706ad38f9
Minha02 <fathumaminha@hmail.com>
2024-07-16 07:26:34 +0530

    2nd Commit

--- 5.1.scala
+++ 5.1.scala
@@ -1,1 +1,32 @@
-import scala.io.StdIn.readline
+import scala.io.StdIn.readLine
+
+object InventoryManagement{
+
+    def getProductList(): List[String] = {
+        def loop(acc: List[String]): List[String] ={
+            val input = readLine("Enter produxt name(or type'done' to finish):")
+            if(input.toLowercase == "done") acc
+            else loop(acc:+ input)
+        }
+        loop(Nil)
+    }
+
+    def printProductList(products: List[String]): Unit ={
+        products.zipWithIndex.foreach{case(product,index) => 
+            println()(s"${index+1}.$product")
+        }
+    }
+
+    def getTotalProducts(products:List[String]) : Int ={
+        products.length
+    }
+
+    def main(args: Array[String]):Unit ={
+        val products = getProductList()
+        println("\nProduct List:")
+        printProductList(products)
+        println("\nTotal number of products : ${getTotalProducts(products)}")
+    }
+
+
+}
