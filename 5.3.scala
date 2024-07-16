26d977851e4179b96ac537e5dc0607b9850eef00
Minha02 <fathumaminha@hmail.com>
2024-07-16 07:48:27 +0530

    5.3.scala commit

---
+++ 5.3.scala
@@ -0,0 +1,32 @@
+object Fibonnaci{
+    def fibonacciRecursive(n: Int):Int = {
+        if(n<=1) n
+        else fibonacciRecursive(n-1) + fibonacciRecursive(n-2)
+    }
+
+    def fibonnaciIterative(n: Int): LIst[Int] ={
+        require(n>=0, "Number of terms must me non-negative")
+        if(n==0) List(0)
+        else if (n==1) List(0,1)
+        else{
+            vaR fibs: List[Int] = List(0,1)
+            for(i<- 2 until n){
+                fibs = fibs :+ (fibs(i-1) + fibs(i-2))
+            }
+            fibs
+        }
+    }
+
+    def printFibonacci(n: Int): Unit ={
+        val fibs = fibonnaciIterative(n)
+        println(s"THe first $n Fibonacci numbers are:")
+        fibs.foreach(println)
+    }
+
+    def main(args: Array[String]): Unit = {
+    println("Enter the number of Fibonacci terms to generate:")
+    val n = scala.io.StdIn.readInt()
+    printFibonacci(n)
+  }
+
+}
